222590347dff1dc48dd8fc2804cb8022902b9ca0
Adetona Sunday <papasunnee@gmail.com>
2018-08-01 12:04:57 +0100

    commit

--- components/Footer.js
+++ components/Footer.js
@@ -6,11 +6,11 @@ export default (props) => {
         <footer>
         <SvgLoader src='/static/images/payment-logo.svg'/>
             <ul>
-                <li><a href="#">Privacy Policy</a></li>
-                <li><a href="#">Terms of Use</a></li>
-                <li><a href="#">Terms of Sale</a></li>
-                <li><a href="#">Cookie Policy</a></li>
-                <li><a href="#">Disclaimer</a></li>
+                <li><a href="/privacy-policy">Privacy Policy</a></li>
+                <li><a href="/terms-of-use">Terms of Use</a></li>
+                <li><a href="/terms-of-sale">Terms of Sale</a></li>
+                <li><a href="/cookie-policy">Cookie Policy</a></li>
+                <li><a href="/disclaimer">Disclaimer</a></li>
             </ul>
             <p>
                 &copy; 2018 PRET. All Right Reserved | Powered by <a target="_blank" href="http://www.altitude-tech.com">Altitude Technology</a>
